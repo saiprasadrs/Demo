60e5f50d268ba0bfcc8605f9a9841298728ae0c9
saiprasadrs <70062873+saiprasadrs@users.noreply.github.com>
2020-08-22 18:44:40 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,6 @@
+pappu = open ("C:\\Users\\Saiprasad Satardekar\\Desktop\\names.xlsx","r")
+print (pappu)
+import pandas as Manish
+sd = Manish.read_excel (r'C:\Users\Saiprasad Satardekar\Desktop\names.xlsx')
+
+print(sd['manish'][7])
